260b862a751d0b3b1da855997a86ebd52e143f8a
vldan <vlad.danilov.020212@gmail.com>
2023-10-17 10:35:04 +0300

    REFACTOR: Вынести навигацию из Header в компонент NavTab

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,12 @@
 import logo from "../../images/logo.svg";
-import profile from "../../images/profile.svg";
+import NavTab from "../NavTab/NavTab";
 
 function Header({ isLoggedIn }) {
   return (
     <header className="header">
       <div className="header__container">
         <img className="header__logo" src={logo} alt="Логотип" />
-        {isLoggedIn && (
-          <div className="header__link-container">
-            <div className="header__films">
-              <p className="header__films-link">Фильмы</p>
-              <p className="header__films-link">Сохранённые фильмы</p>
-            </div>
-            <img
-              src={profile}
-              alt="Ссылка на акканут"
-              className="header__profile-link"
-            />
-          </div>
-        )}
+        {isLoggedIn && <NavTab />}
         {!isLoggedIn && (
           <div className="header__links">
             <a href="#" className="header__link-register">
