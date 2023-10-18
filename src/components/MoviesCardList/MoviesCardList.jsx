d992ee06d95087e79ef835b331b3142d678bc2cd
vldan <vlad.danilov.020212@gmail.com>
2023-10-18 14:46:34 +0300

    FEAT: Добавить в компонент MoviesCardList кнопку 'Ещё'

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -3,18 +3,23 @@ import MoviesCard from "../MoviesCard/MoviesCard";
 function MoviesCardList() {
   return (
     <section className="cards-list">
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
+      <div className="cards-list__container">
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+      </div>
+      <div className="cards-list__btn-container">
+        <button className="cards-list__btn-more">Ещё</button>
+      </div>
     </section>
   );
 }
