eb0e0642cbe359e96d5ea3cb9d4ae92f9841a85a
vldan <vlad.danilov.020212@gmail.com>
2023-10-23 09:38:06 +0300

    FEAT: Создать компонент Prelodaer

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,11 +1,13 @@
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import Preloader from "../Preloader/Preloader";
 
 function Movies() {
   return (
     <>
       <SearchForm />
-      <MoviesCardList  />
+      <Preloader />
+      <MoviesCardList />
     </>
   );
 }
