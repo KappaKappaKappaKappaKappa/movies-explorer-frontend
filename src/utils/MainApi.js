56b65cf34347c627a391dff34a14a85e9f5d41fe
vldan <vlad.danilov.020212@gmail.com>
2023-11-09 16:10:56 +0300

    FEAT: Добавить функции для получения и добавления сохраненных карточек

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -4,6 +4,8 @@ const headers = {
 
 const BASE_URL = "http://localhost:3000";
 
+const token = localStorage.getItem("jwt");
+
 const checkResponse = (res) => {
   if (res.ok) {
     return res.json();
@@ -16,7 +18,7 @@ export const getUserInfo = () => {
     method: "GET",
     headers: {
       ...headers,
-      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      Authorization: `Bearer ${token}`,
     },
   }).then(checkResponse);
 };
@@ -26,11 +28,44 @@ export const updateUserInfo = (data) => {
     method: "PATCH",
     headers: {
       ...headers,
-      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({
       name: data.name,
       email: data.email,
     }),
+  }).then(checkResponse);
+};
+
+export const saveMovie = (data) => {
+  return fetch(`${BASE_URL}/movies`, {
+    method: "POST",
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      nameRU: data.nameRU,
+      nameEN: data.nameEN,
+      country: data.country,
+      duration: data.duration,
+      director: data.director,
+      year: data.year,
+      description: data.description,
+      image: `https://api.nomoreparties.co/${data.image.url}`,
+      trailerLink: data.trailerLink,
+      thumbnail: `https://api.nomoreparties.co/${data.image.formats.thumbnail.url}`,
+      movieId: data.id,
+    }),
+  }).then(checkResponse);
+};
+
+export const getSavedMovies = () => {
+  return fetch(`${BASE_URL}/movies`, {
+    method: "GET",
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${token}`,
+    },
   }).then(checkResponse);
 };
