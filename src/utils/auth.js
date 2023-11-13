460a72f2d55956bd5bae514a620b1ee0a24c1653
vldan <vlad.danilov.020212@gmail.com>
2023-11-13 21:36:27 +0300

    FIX: Исправить адрес для обращения к серверу

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:3000";
-const BASE_URL = "http://api.mov.explorer.nomoredomainsrocks.ru/";
+const BASE_URL = "http://api.mov.explorer.nomoredomainsrocks.ru";
 
 const headers = {
   "Content-Type": "application/json",
